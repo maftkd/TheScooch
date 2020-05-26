ba7e0afe50d2d672e528a9d4f99626f49261def1
Michael <michael.feldman09@gmail.com>
2020-05-26 18:27:50 -0400

    Updated gitignore added project files and scripts

---
+++ TheScoochUnity/Assets/Scooch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scooch : MonoBehaviour
+{
+    public float _moveSpeed;
+    public float _winPos;
+
+    public enum GameStates { PLAY, WIN }
+    public GameStates _state = GameStates.PLAY;
+
+    public enum StealthStates { CHILL, SUSPECT, BUSTED}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_state)
+        {
+            case GameStates.PLAY:
+                if (Input.anyKeyDown)
+                {
+                    Move();
+                }
+                break;
+            case GameStates.WIN:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Move()
+    {
+        transform.localPosition += Vector3.left * _moveSpeed;
+        if(transform.localPosition.x<= _winPos)
+        {
+            Win();
+        }
+    }
+
+    private void Win()
+    {
+        Debug.Log("Win");
+        _state = GameStates.WIN;
+    }
+}
